5482f46b26a5b754cf24166024418016a4b19eb1
Tales Patric <taleshy1@gmail.com>
2023-05-29 00:40:32 -0300

    Projeto finalizado

---
+++ labecommerce/src/components/Loading/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import loadingImage from "../../assets/loadingImage.gif.gif"
+import { LoadingBox } from "./style";
+
+export default function LoadingPage() {
+
+    return (
+        <LoadingBox>
+            <img src={loadingImage} />
+        </LoadingBox>
+    )
+}
---
+++ labecommerce/src/components/Loading/style.js
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const LoadingBox = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img{
+        width: 20%;
+    }
+
+` 
+
